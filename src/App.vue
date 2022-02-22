150e8c281b1dc5abb9b8b72bea11fe7c6f17a87d
GuerdySSS <sanya.lyubimov.9500@mail.ru>
2022-02-22 19:05:08 +0300

    adaptive done, fixed bugs

--- src/App.vue
+++ src/App.vue
@@ -43,6 +43,7 @@ export default {
       setInterval(() => {
         let myBirthay = new Date(`${year}-03-30T00:00:00.000`).getTime()
         let delta = Math.floor((myBirthay - new Date()) / 1000)
+
         let days = Math.floor(delta / 86400)
         delta -= days * 86400
 
@@ -59,7 +60,7 @@ export default {
         this.minutes = minutes
         this.seconds = seconds
 
-        if (delta == 0) ++year
+        if (days == 0 && hours == 0 && minutes == 0 && seconds == 0) year += 1
       }, 1000)
     }
   }
@@ -81,23 +82,40 @@ export default {
     &__header {
       text-align: center;
       font-size: 50px;
+
+      @media(max-width: 540px) {
+        font-size: 30px;
+      }
     }
 
     &__timer {
       display: flex;
       justify-content: space-between;
-      width: 700px;
+      max-width: 700px;
       margin: 50 auto;
+      flex-wrap: wrap;
+
+      @media(max-width: 540px) {
+        justify-content: space-around;
+      }
 
       &__block {
         text-align: center;
 
         &__counter {
           font-size: 90px;
+
+          @media(max-width: 540px) {
+            font-size: 60px;
+          }
         }
 
         &__text {
           font-size: 35px;
+
+          @media(max-width: 540px) {
+            font-size: 20px;
+          }
         }
       }
     }
